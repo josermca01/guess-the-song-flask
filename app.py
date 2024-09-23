0b9e4c8ca5397026ba902fc95027a7cdd766e915
josermca01 <josermca01@gmail.com>
2024-09-23 15:23:00 -0300

    Change CORS settings

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 import python  # Import your Python script
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/get_playlist/*": {"origins": "http://localhost:5173/*"}})
+
 
 @app.route('/get_playlist/<int:playlist_id>', methods=['GET'])
 def get_playlist(playlist_id):
     text = python.main(playlist_id)
     response = jsonify(text)
-    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
 if __name__ == '__main__':
