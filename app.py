f97c669108255a4c0164747f12cb58dfe570b7e2
josermca01 <josermca01@gmail.com>
2024-09-23 15:43:23 -0300

    Add front-end urls to CORS config

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 import python  # Import your Python script
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url_front_local = os.getenv("URL_FRONT_LOCAL")
+url_front_remote = os.getenv("URL_FRONT_REMOTE")
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/get_playlist/*": {"origins": "http://localhost:5173/*"}})
+cors = CORS(app, resources={r"/get_playlist/*": {"origins": [url_front_local,url_front_remote]}})
 
 
 @app.route('/get_playlist/<int:playlist_id>', methods=['GET'])
